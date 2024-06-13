23d7a0a1c71557a9b2885bb265ecb76caf792f07
Gaurav Singh <gaurav.singh@s2infinitum.com>
2024-06-13 23:07:57 +0530

    ww

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'anc-record';
+
+  showNavbar :boolean = false;
+  menuToggler(){
+    this.showNavbar = !this.showNavbar;
+  }
 }
